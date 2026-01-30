f46e15565c3061658dd6342806c814216e4c2d25
Matthew Berens <matthewjberens@gmail.com>
2026-01-30 14:12:30 -0500

    Adding new streamstats data and data wrangling script.

--- code/get_streamstats.R
+++ code/get_streamstats.R
@@ -3,25 +3,44 @@
 # Name: Matthew Berens
 # Updated: 1/28/2026
 #############################################
+library(magrittr)
+library(dplyr)
 
 # get list of streamstats csv exports
 streamstats_path <- list.files(path = "data/streamstats", pattern = "*.csv", full.names = TRUE)
 
-
 # define function to read in streamstats files
 import_streamstats <- function(file_path) {
-  data <-  read.csv(file_path, header = TRUE) %>%
-    select(-c(Parameter.Description, Unit)) %>%
-    pivot_wider(names_from = Parameter.Code, values_from = Value) %>%
+  
+  data <-  read.csv(file_path, header = FALSE)
+  
+  latitude <- data[4,2]
+  longitude <- data[5,2]
+  GNIS_id <- data[7,2]
+  GNIS_name <- data[8,2]
+  
+  data <- data[(12:66),c(1,3)] %>%
+    
+    janitor::row_to_names(row_number = 1) %>%
+    rename(parameter.code = 'Parameter Code') %>%
+    tidyr::pivot_wider(names_from = parameter.code, values_from = Value) %>%
     mutate(site.number = 
-             sub("_.*", "", fs::path_ext_remove(basename(file_path)))
-    )
+             sub("_.*", "", fs::path_ext_remove(basename(file_path)))) %>%
+    
+    mutate(latitude = latitude,
+           longitude = longitude,
+           GNIS_id = GNIS_id,
+           GNIS_name = GNIS_name) %>%
+    
+    relocate(site.number, latitude, longitude) %>%
+    mutate(across(latitude:SSURGOD, ~ as.numeric(as.character(.)))) %>%
+    
+    mutate(RELELEV = ELEV - OUTLETELEV)
+  
 }
 
 # combine all streamstats files into single data frame
-vwin.streamstats <- do.call(rbind, lapply(streamstats_path, import_streamstats)) %>%
-  relocate(site.number) %>%
-  mutate(RELELEV = ELEV - OUTLETELEV)
+vwin.streamstats <- do.call(rbind, lapply(streamstats_path, import_streamstats))
 
 # remove file path and function
 rm(streamstats_path, "import_streamstats")
@@ -33,6 +52,16 @@ vwin.site.data <- read_csv("data/raw/VWIN_site_data.csv") %>%
   
   # select out only the variables of interest
   select(river.basin, water.body, county, site.number) %>%
+  
+  unique() %>%
 
   # merge with streamstats information
   merge(vwin.streamstats, by = "site.number")
+
+rm(vwin.streamstats)
+
+
+
+
+
+
